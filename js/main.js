706b59351faa30b0446b2c2278ad02ae2c81d992
AleksF2022 <aleksfomin52@gmail.com>
2023-04-05 18:05:55 +0300

    header-slider completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-
+  $('.header-slider').slick({
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    vertical: true,
+    verticalSwiping: true,
+    dots: true,
+    dotsClass: 'header-dots'
+  });
 });
